46fb9fb10d0c88318be061d588fb0c8daa583079
Brilock <artem_zezyulin@mail.ru>
2022-07-17 10:00:24 +0300

    Added configurable start (port - env variable)

--- main.go
+++ main.go
@@ -4,14 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/brilock/FirstService/handlers"
 )
 
 func main() {
 	log.Print("Starting the service...")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
+
 	router := handlers.Router()
-	http.HandleFunc("/home", httpHandler)
+	//http.HandleFunc("/home", httpHandler)
 
 	log.Print("The service is ready to listen and serve.")
 	log.Fatal(http.ListenAndServe(":8000", router))
